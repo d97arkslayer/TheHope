135f4fb09b6a92110d1198ca6a973fcbb6e2b92f
d97arkslayer <esteban.herrera.1126@gmail.co m>
2018-05-04 22:51:31 -0500

    CRUD estudiantes, este es una prueba de "Cacharreo"

--- app/Controllers/Http/StudentController.js
+++ app/Controllers/Http/StudentController.js
@@ -1,26 +1,82 @@
 'use strict'
-
+const Student = use('App/Models/Student')
 class StudentController {
-  async index () {
-  }
+    async index({ response }) {
+        const students = await Student.all()
 
-  async create () {
-  }
+        response.status(200).json({
+            message: 'Here are your students.',
+            data: students
+        })
+    }
 
-  async store () {
-  }
+    async create() {}
 
-  async show () {
-  }
+    async store({ request, response, params: { id } }) {
+        const { name, document, age, email } = request.post()
+        const student = await Student.create({ name, document, age, email })
+        response.status(201).json({
+            message: 'Successfully created a new Student.',
+            data: student
+        })
+    }
 
-  async edit () {
-  }
+    async show({ response, params: { id } }) {
 
-  async update () {
-  }
+        const student = await Student.find(id)
+        if (student) {
+            response.status(200).json({
+                message: 'Here is your student.',
+                data: student
+            })
+        } else {
+            response.status(404).json({
+                message: 'Student not found.',
+                data: id
+            })
+        }
 
-  async destroy () {
-  }
+
+    }
+
+    async edit() {}
+
+    async update({ request, response, params: { id } }) {
+        const student = await Student.find(id)
+        if (student) {
+            const { name, document, age, email } = request.post()
+            student.name = name
+            student.document = document
+            student.age = age
+            student.email = email
+            await student.save()
+            response.status(200).json({
+                message: 'Successfully updated this student.',
+                data: student
+            })
+        } else {
+            response.status(200).json({
+                message: 'Student not found.',
+                id
+            })
+        }
+    }
+
+    async destroy({ response, params: { id } }) {
+        const student = await Student.find(id)
+        if (student) {
+            await student.delete()
+            response.status(200).json({
+                message: 'Successfully deleted this student.',
+                id
+            })
+        } else {
+            response.status(200).json({
+                message: 'Student not found.',
+                id
+            })
+        }
+    }
 }
 
 module.exports = StudentController
--- start/routes.js
+++ start/routes.js
@@ -16,5 +16,11 @@
 const Route = use('Route')
 
 Route.get('/', ({ request }) => {
-  return { greeting: 'Hello world in JSON' }
+    return { greeting: 'Hello world in JSON' }
 })
+
+Route.get('students', 'StudentController.index')
+Route.get('students/:id', 'StudentController.show')
+Route.post('students', 'StudentController.store')
+Route.patch('students/:id', 'StudentController.update')
+Route.delete('students/:id', 'StudentController.destroy')
